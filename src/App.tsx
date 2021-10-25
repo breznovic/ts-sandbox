769a6dad21b8148624d8ec8ccd34e7acb871d10a
Breznovic <breznovic@mail.ru>
2021-10-25 19:49:18 +0300

    Beginning

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+
+const [number, setNumber] = useState(5)
+
+const changeNumber = () => {
+  setNumber(10)
+} 
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
